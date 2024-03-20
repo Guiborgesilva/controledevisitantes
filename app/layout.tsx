b36f73b645cf5792a6091c25d6006f660941c8fb
Guiborgesilva <guiborgesilva@outlook.com>
2024-03-20 15:02:36 -0300

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Controle de Visitantes",
+  description: "Um app para controle interno de Visitantes nos nossos cultos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
