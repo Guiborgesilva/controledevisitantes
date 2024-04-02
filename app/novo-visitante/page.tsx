03704c22e85c934ae561557284e2d28ac102fc65
Guiborgesilva <guiborgesilva@outlook.com>
2024-04-02 01:44:04 -0300

    Voltando ao UUID

--- app/novo-visitante/page.tsx
+++ app/novo-visitante/page.tsx
@@ -214,6 +214,7 @@ export default function Page() {
               p-2
               mt-3
               rounded-[10px]
+              font-bold
               hover:bg-white
               hover:text-black
               transition
